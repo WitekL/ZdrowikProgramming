d09cad5f169d8ae9a45354560c1ac4d744ff0fd5
WitekL <witold.leicht@gmail.com>
2018-03-03 23:07:20 +0100

    Created streak controller with all required actions and views

--- app/controllers/streaks_controller.rb
+++ app/controllers/streaks_controller.rb
@@ -1,7 +1,30 @@
-def StreaksController < ApplicationController
+class StreaksController < ApplicationController
   def index
     # @number_of_days = he_did_programm ? postive_streak_days : negative_streak_days
 
-    @no_of_days = NegativeStreak
+    @streak = Streak.last
+    @created_at = @streak.created_at
+
+    @no_of_days = ((Time.zone.now - @created_at.to_i).to_i/ 1.day) + 1
+  end
+
+  def new
+    @streak = Streak.new
+  end
+
+  def create
+    @streak = Streak.new(streak_params)
+
+    if @streak.save
+      redirect_to
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def streak_params
+    params.require(:streak).permit(:positive)
   end
 end

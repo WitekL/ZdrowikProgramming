509d1c5750adb4068b6112ebee335f9343a3921f
WitekL <witold.leicht@gmail.com>
2018-03-03 21:09:26 +0100

    Created negative streak migration

--- db/migrate/20180303194136_create_negative_streaks.rb
+++ db/migrate/20180303194136_create_negative_streaks.rb
@@ -1,8 +1,6 @@
 class CreateNegativeStreaks < ActiveRecord::Migration[5.1]
   def change
     create_table :negative_streaks do |t|
-      t.datetime :beginning
-
       t.timestamps
     end
   end
